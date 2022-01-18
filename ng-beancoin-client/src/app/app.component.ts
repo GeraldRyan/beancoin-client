aea4ce6a44836ed1a3340ca519bccbf1c5c41808
GeraldRyan <gerald.ryan40@yahoo.com>
2022-01-18 00:59:31 -0600

    micro front end set up working with react and angular apps. Serves all if/when js script files are being served or available

--- ng-beancoin-client/src/app/app.component.ts
+++ ng-beancoin-client/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ng-beancoin-client-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'ng-beancoin-client';
--- ng-beancoin-client/src/test.ts
+++ ng-beancoin-client/src/test.ts
@@ -4,24 +4,21 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
+  platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
-    <T>(id: string): T;
+  context(path: string, deep?: boolean, filter?: RegExp): {
     keys(): string[];
+    <T>(id: string): T;
   };
 };
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(),
+  { teardown: { destroyAfterEach: true }},
 );
 
 // Then we find all the tests.
