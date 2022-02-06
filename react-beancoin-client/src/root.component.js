286ba5b23aa9190a73851a735230cf93d8ae86f5
GeraldRyan <gerald.ryan40@yahoo.com>
2022-02-06 15:33:28 -0600

    starting react program

--- react-beancoin-client/src/root.component.js
+++ react-beancoin-client/src/root.component.js
@@ -1,5 +1,33 @@
 import './index.css'
+import { useState } from 'react';
 
 export default function Root(props) {
-  return <section className="App">{props.name} is mounted and girls are cute!</section>;
+
+  const [userQuery, setUserQuery] = useState()
+
+  const updateUserQuery = event =>{
+    console.log("user query: ", userQuery)
+    setUserQuery(event.target.value)
+  }
+
+  const searchQuery = () =>{
+    window.open(`https://www.google.com/search?q=${userQuery}`)
+  }
+
+  const handleKeyPress = event =>{
+    if (event.key == "Enter"){
+      searchQuery();
+    }
+  }
+
+  return (
+<div>
+      <section className="App">{props.name}
+        is mounted and girls are cute!!
+      </section>
+
+      <input value={userQuery} onChange={updateUserQuery}/>
+      <button onClick={searchQuery}>Search</button>
+</div>
+  );
 }
